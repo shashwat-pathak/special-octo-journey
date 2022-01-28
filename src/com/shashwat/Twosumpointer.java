67da0e646f3637bf4f513cae912ed91828eaaf4e
Shashwat Pathak <shashwatpathak98@gmail.com>
2022-01-28 23:16:16 +0530

    feat: :drum: Created Twosumpointer.java

---
+++ src/com/shashwat/Twosumpointer.java
@@ -0,0 +1,25 @@
+package com.shashwat;
+
+public class Twosumpointer {
+    public static boolean hasSum(int[] arr, int target){
+        int left = 0;
+        int right = arr.length-1;
+        boolean hasSum = false;
+        while(left<right) {
+            if(arr[left]+arr[right]<target) left++;
+            else if(arr[left]+arr[right]>target) right--;
+            else {
+                hasSum = true;
+                break;
+            }
+        }
+        return hasSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr =  {1,2,3,6,9};
+        int target = 15;
+        boolean status = hasSum(arr,target);
+        System.out.println("The array has the given sum: "+status);
+    }
+}
